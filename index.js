2728b49fe0aabb79878c681c8e5881c8c7596d2c
Vlad <vladdp70@gmail.com>
2021-05-22 19:39:35 +0300

    Собстевенный map

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const a = 4;
+function MyArrayProto() {
+  this.map = function (cb) {
+    const result = new MyArray();
+
+    for (let i = 0; i < this.length; i++) {
+      const cbResult = cb(this[i], i, this);
+      result.push(cbResult);
+    }
+  };
+}
+
+function MyArray(...args) {}
+
+MyArray.prototype = new MyArrayProto();
+
+const meArray = new MyArray(
+  {
+    age: 18,
+    name: "test",
+  },
+  {
+    age: 21,
+    name: "test2",
+  }
+);
